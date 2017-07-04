ab58f609336dceda92fef681ff8245ec7277e24e
R.J. Steinert <rj@rjsteinert.com>
2017-07-04 09:38:55 -0400

    Refact angular-pages-build.sh to put all page components in a single Pages module

--- angular-pages-build.sh
+++ angular-pages-build.sh
@@ -1,85 +1,74 @@
 #!/bin/bash
 CLI_DIR=$1
-SOURCE_DIR=$PWD
-APP_DIR="$SOURCE_DIR/code/src/app"
-DIST_DIR="$SOURCE_DIR/code/dist"
+PROJECT_DIR=$PWD
 
-# Clean up.
-rm -r public
-rm -r code
-
-# Create the new app.
-ng new --routing code 
+# Remove previously created pages module and regenerate it..
+rm -r "$PROJECT_DIR/src/app/pages"
+cd "$PROJECT_DIR/src/app" && ng generate module --routing pages 
 
 # Get all MD file paths.
+cd "$PROJECT_DIR/src/pages"
 index=0
 for x in `find ./ * | grep -v "code\/" | grep -v "\.\/\/" | grep ".md"`; do
     MD_PATHS[$index]="$x"
     index=$(($index+1))
 done 
+cd 
 
 # Convert them all to HTML
+cd "$PROJECT_DIR/src/pages"
 index=0
 for MD_PATH in "${MD_PATHS[@]}"
 do
   :
   HTML_PATH=`echo $MD_PATH | sed -e 's/.md/.html/'` 
-  HTML_PATHS[$index]="$HTML_PATH"
 	echo "Transpiling Markdown at $MD_PATH to HTML at $HTML_PATH"
-  ## TODO: Transpile anchor tags into NG Router links.
   cat "$MD_PATH" | mdown > "$HTML_PATH"
 	index=$(($index+1))
 done
 
-# Use index.html to override app.component.html
-rm "$APP_DIR/app.component.html"
-cp "$SOURCE_DIR/index.html" "$APP_DIR/app.component.html"
+# Get all HTML file paths.
+cd "$PROJECT_DIR/src/pages"
+index=0
+for x in `find ./ * | grep -v "code\/" | grep -v "\.\/\/" | grep ".html"`; do
+    HTML_PATHS[$index]="$x"
+    index=$(($index+1))
+done 
 
-# Generate a Module, Component, and Route for each HTML file.
+# TODO: Transpile anchor tags into NG Router links.
+
+# Generate a Component and Route for each HTML file.
 index=0
 for HTML_PATH in "${HTML_PATHS[@]}"
 do
   :
-  cd "$APP_DIR"
-  FS_NAME=`echo $HTML_PATH | sed -e 's/.html//' | sed -e 's/\//--/'`
-  MODULE_PATH="$APP_DIR/$FS_NAME"
-  MD_PATH=${MD_PATHS[$index]}
-  ESCAPED_MD_PATH=`echo "$MD_PATH" | sed -e ''`
-  echo "Installing module: $FS_NAME to $MODULE_PATH"
-  ng generate module --routing $FS_NAME
-  MODULE_NAME=`cat "$APP_DIR/$FS_NAME/$FS_NAME.module.ts" | tail -n 1 | awk '{print $3}'`
-  COMPONENT_NAME=`echo "$MODULE_NAME" | sed -e "s/Module/Component/"`
-  # ES6 import module into app.module.ts.
-  echo "import { $MODULE_NAME } from './$FS_NAME/$FS_NAME.module';" > tmp
-  cat "$APP_DIR/app.module.ts" >> tmp
-  mv tmp "$APP_DIR/app.module.ts"
-  # Angular import the module into the AppModule.
-  cat "$APP_DIR/app.module.ts" | sed -e "s/imports\: [\[]/imports\: [ $MODULE_NAME,/" > "$APP_DIR/app.module.ts"
-  # Generate component.
-  cd "$MODULE_PATH" 
-  ng generate component $FS_NAME;
-  # Move HTML file to Component.
-  cd $SOURCE_DIR 
-  mv "$HTML_PATH" "$MODULE_PATH/$FS_NAME/$FS_NAME.component.html"
-  # Edit routing to ES6 import Component.
-  echo "import { $COMPONENT_NAME } from './$FS_NAME/$FS_NAME.component';" > tmp
-  cat "$MODULE_PATH/$FS_NAME-routing.module.ts" >> tmp
-  mv tmp "$MODULE_PATH/$FS_NAME-routing.module.ts"
-  # Edit routing to declare route or Component.
-  echo "MD_PATH IS $MD_PATH"
-  cat "$MODULE_PATH/$FS_NAME-routing.module.ts" | sed -e "s#const routes# const routes \: Routes \ = \[ \{ path\: '$MD_PATH', component\: $COMPONENT_NAME \}\] \/\/#" > tmp
-  mv tmp "$MODULE_PATH/$FS_NAME-routing.module.ts"
+
+  # Generate the Component's machine name for the file system. Convert forward slashes to double dashes. 
+  COMPONENT_MACHINE_NAME=`echo $HTML_PATH | sed -e 's/.html//' | sed -e 's/\//--/'`
+
+  # ESCAPED_MD_PATH=`echo "$MD_PATH" | sed -e ''`
+
+  # Generate the Component.
+  cd "$PROJECT_DIR/src/app/pages"
+  ng generate component $COMPONENT_MACHINE_NAME
+
+  # Get the Component's Class Name that ng generated.
+  COMPONENT_CLASS_NAME=`cat "$PROJECT_DIR/src/app/pages/$COMPONENT_MACHINE_NAME/$COMPONENT_MACHINE_NAME.component.ts" | tail -n 1 | awk '{print $3}'`
+
+  # Copy the HTML file to Component's HTML file.
+  cp "$PROJECT_DIR/src/pages/$HTML_PATH" "$PROJECT_DIR/src/app/pages/$COMPONENT_MACHINE_NAME/$COMPONENT_MACHINE_NAME.component.html"
+
+  # Edit Pages Module's routing to ES6 import Component.
+  cd $PROJECT_DIR
+  echo "import { $COMPONENT_CLASS_NAME } from './$COMPONENT_MACHINE_NAME/$COMPONENT_MACHINE_NAME.component';" > tmp
+  cat "$PROJECT_DIR/src/app/pages/pages-routing.module.ts" >> tmp
+  mv tmp "$PROJECT_DIR/src/app/pages/pages-routing.module.ts"
+
+  # Edit routing of the Pages Module to declare route for Component.
+  cd $PROJECT_DIR
+  cat "$PROJECT_DIR/src/app/pages/pages-routing.module.ts" | sed -e "s#const routes# const routes \: Routes \ = \[ \\n\{ path\: '$HTML_PATH', component\: $COMPONENT_CLASS_NAME \}\] \/\/#" > tmp
+  mv tmp "$PROJECT_DIR/src/app/pages/pages-routing.module.ts"
 	index=$(($index+1))
 done
 
-# Build the Angular App.
-cd "$SOURCE_DIR/code" 
-ng build
-# Move the dist directory to home.
-cd ..
-mv ./code/dist ./public
-# Cleanup
-# rm -rf code
-# Start the server.
-cd public
-hs
+# TODO: Add the index component to the '' route.
